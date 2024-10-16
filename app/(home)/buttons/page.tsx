72c2e700c0bf682565034279aaf7c18b821068a7
AdrienMoullec <s4209664@glos.ac.uk>
2024-10-16 16:26:29 +0100

    Screen and images

--- app/(home)/buttons/page.tsx
+++ app/(home)/buttons/page.tsx
@@ -1,13 +1,28 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 const ButtonsPage = () => {
     return (
-        <div className="p-5 space-y-5 flex-col flex max-w-[200px] bg-cyan-500 tracking-wide ">
-            <Button variant={"ghost"} className="hover:bg-red-400">Primary</Button>
-            <Button>Primary outline</Button>
-            <p>
-                Help I am on da moon
+        <div className="p-5 space-y-5 flex-col flex max-w-[600px] bg-cyan-500 tracking-wide">
+            <Button variant={"ghost"} className="hover:bg-red-400">WTH???</Button>
+            <Button>
+                <Image
+                    src={"/MoonMan.png"}
+                    width={25}
+                    height={25}
+                    alt="VECTOR"
+                />
+                HOW DID I GET HERE???
+            </Button>
+            <p className="centre">
+                I am on da moon
             </p>
+            <Image
+                src={"/MoonMan.png"}
+                width={250}
+                height={25}
+                alt="VECTOR"
+            />
         </div>
     );
 };
